9c38b6ef6cacc295cc163923858734729549b1e7
jellyjul <100496@mail.ru>
2023-07-07 14:39:35 +0300

    started slider

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../components/Card/Card";
 import styles from "../styles/game.module.scss"
 import cards from "../data/table-row.json"
 
 export default function Game(){
-    return(
-        <div className={styles.container}>
-        {cards.map((card,id )=>
-            <Card 
-            id={id} 
-            english={card.english} 
-            transcription= {card.transcription} 
-            russian = {card.russian}
-            tags = {card.tags}
-            tags_json = {card.tags_json} />)}
-            </div>
-    )
-}
-    
 
+const [position,setPosition] = useState(0)
+
+const onShowPrevious = () => {
+    if (position > 0) {
+        setPosition(position-1)
+    }
+}
+
+const onShowNext = () => {
+    setPosition(position+1)
+}
+
+    return(
+        <div className={styles.game}>
+        <div className= {styles.game__container}>
+            <button className={styles.game__button} onClick={onShowPrevious} >PREVIOUS</button>
+            <div className={styles.container}>
+                {cards.map((card,id )=>
+                    <Card 
+                    id={id} 
+                    english={card.english} 
+                    transcription= {card.transcription} 
+                    russian = {card.russian}
+                    tags = {card.tags}
+                    tags_json = {card.tags_json} />)}
+            </div>
+            <button className={styles.game__button} onClick={onShowNext}>NEXT</button>
+            
+        </div>
+        <div className={styles.game__learnt}>Words learnt: / </div>
+        <div className={styles.game__swiper}>
+        </div>
+        </div>
+)}
+    
+    
